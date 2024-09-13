58f62fa1103c13ad5f6946a16615b9428c0aaddf
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-09-13 12:01:13 -0300

    feat: trabalhe conosco

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ window.addEventListener('click', function(event) {
     }
 });
 
+// Script para abrir e fechar o pop-up de contato
+document.getElementById('work-button').addEventListener('click', function() {
+    document.getElementById('work-popup').style.display = 'flex';
+});
+
+document.querySelector('.close-button-work').addEventListener('click', function() {
+    document.getElementById('work-popup').style.display = 'none';
+});
+
+// Fechar o pop-up ao clicar fora do conteúdo
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('work-popup')) {
+        document.getElementById('work-popup').style.display = 'none';
+    }
+});
+
+
+
 // Script para alternar o menu de navegação em dispositivos móveis
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('show');
